9c3db52dd9e09afdb4d8dbff857ac8bfeeca56fa
Miguel Ferreira <miguelfferreira93@gmail.com>
2018-03-24 13:36:42 +0000

    Mostrando as Categorias

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -1,3 +1,4 @@
 export const API_CONFIG = {
-    baseUrl: "http://localhost:8090" 
+    baseUrl: "http://localhost:8090",
+    bucketBaseUrl : "https://s3.eu-west-2.amazonaws.com/java-spring-ionic" 
 }
